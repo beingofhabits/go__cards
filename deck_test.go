274d87926365e979cb2a9903014a1fc1156a318e
beingofhabits <beingofhabits@gmail.com>
2018-11-11 21:51:11 +0300

    simple test

--- deck_test.go
+++ deck_test.go
@@ -8,6 +8,6 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Error("Expected deck len of 20, but got ", string(len(d)))
+		t.Errorf("Expected deck len of 16, but got %v", string(len(d)))
 	}
 }
