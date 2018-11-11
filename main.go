f26c6849bd8b08d622580b35fa5c3a25c28f8612
beingofhabits <beingofhabits@gmail.com>
2018-11-11 19:47:16 +0300

    deal func

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"ace of diamonds", newCard()}
-	cards = append(cards, "six of spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "five of diamonds"
 }
