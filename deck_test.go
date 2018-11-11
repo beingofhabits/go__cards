a044eee1068f6c7b456ad9a8b8b3c15277fb5938
beingofhabits <beingofhabits@gmail.com>
2018-11-11 22:13:56 +0300

    tested files operations

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -16,4 +17,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last value of deck to be of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 card, but got %v ", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
