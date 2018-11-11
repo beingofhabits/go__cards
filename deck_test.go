3e0e82c98e9aef6ef149d596dab1354da477d2af
beingofhabits <beingofhabits@gmail.com>
2018-11-11 21:56:15 +0300

    NewDeck Tested

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck len of 16, but got %v", string(len(d)))
 	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected zero value of deck to be Ace of Spades, but got %v", d[0])
+	}
+	if d[3] != "Four of Clubs" {
+		t.Errorf("Expected last value of deck to be of Four of Clubs, but got %v", d[3])
+	}
 }
