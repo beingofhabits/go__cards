c9898631d2f476853d3027017cd7971c55accc6a
beingofhabits <beingofhabits@gmail.com>
2018-11-11 19:14:36 +0300

    few changes

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	cards := []string{"ace of diamonds", newCard()}
+	cards = append(cards, "six of spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "ficarde of diamonds"
 }
