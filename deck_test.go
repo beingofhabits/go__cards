14b87d104bc78d66e3ffd6d56cb3d5473db8e1b3
beingofhabits <beingofhabits@gmail.com>
2018-11-11 21:49:04 +0300

    simple test

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Error("Expected deck len of 20, but got ", string(len(d)))
+	}
+}
